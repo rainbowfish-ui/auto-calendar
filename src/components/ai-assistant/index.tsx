e2ea388d56ad34e79c22606cdd4399585ee43fe5
subhash131 <subhashnayak131@gmail.com>
2025-01-28 02:15:23 +0530

    create component: AI Assistant

---
+++ src/components/ai-assistant/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AiAssistant = () => {
+  return (
+    <div className="absolute left-16 bottom-0 px-4 py-2 bg-[#F4F5F7] z-10 border rounded-t-lg w-80 h-10 hover:h-[30rem] transition-all">
+      <h4>AI Assistant</h4>
+    </div>
+  );
+};
+
+export default AiAssistant;
