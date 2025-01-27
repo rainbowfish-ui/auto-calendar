e0c34bfc0d87100176fe5fd4d35511d0bff4d23f
subhash131 <subhashnayak131@gmail.com>
2025-01-28 02:28:21 +0530

    modularize components

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-import MainView from "./main-view";
-import AiAssistant from "../ai-assistant";
+import Summary from "../summary";
+import Header from "./header";
+import ActiveView from "./active-view";
 
 const Calendar = () => {
   return (
     <div className="size-full relative max-h-screen overflow-hidden">
-      <MainView />
+      <div className="w-full h-screen overflow-hidden flex pr-10">
+        <Summary />
+        <div className="h-fit w-full flex justify-center items-center flex-col pl-4">
+          <Header />
+          <ActiveView />
+          <div className="h-24 w-full"></div>
+        </div>
+      </div>
     </div>
   );
 };
