84f11e176eca848b4afb53799b71ac4bc28758f2
subhash131 <subhashnayak131@gmail.com>
2025-01-28 01:51:00 +0530

    update sidebar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,32 +1,52 @@
+"use client";
 import React from "react";
+import Link from "next/link";
 import { CiCalendar } from "react-icons/ci";
 import { GoTasklist } from "react-icons/go";
 import { IoIosSearch } from "react-icons/io";
 import { PiShootingStarLight } from "react-icons/pi";
+import { usePathname } from "next/navigation";
+
+const navItems = [
+  {
+    name: "Search",
+    href: "#",
+    icon: IoIosSearch,
+  },
+  {
+    name: "Calendar",
+    href: "/calendar",
+    icon: CiCalendar,
+  },
+  {
+    name: "Tasks",
+    href: "/tasks",
+    icon: GoTasklist,
+  },
+];
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
-    <div className="w-16 h-full bg-[#F4F5F7] flex flex-col items-center justify-start py-4 gap-6 border-r">
+    <div className="w-16 h-full bg-[#F4F5F7] flex flex-col items-center justify-start py-4 gap-6 border-r z-50">
       <PiShootingStarLight size={30} />
       <nav className="size-full flex flex-col gap-2 items-start p-2">
-        <button className="rounded-lg p-3 relative group">
-          <div className="w-fit h-fit rounded-md absolute bg-[#D6D7E0] text-gray-800 left-12 top-1 text-xs p-2 hidden group-hover:block">
-            Search
-          </div>
-          <IoIosSearch size={18} />
-        </button>
-        <button className="bg-[#D6D7E0] rounded-lg p-3 relative group">
-          <div className="w-fit h-fit rounded-md absolute left-12 top-1 text-xs text-gray-800 bg-[#D6D7E0] p-2 hidden group-hover:block">
-            Calendar
-          </div>
-          <CiCalendar size={18} />
-        </button>
-        <button className="rounded-lg p-3 relative group">
-          <div className="w-fit h-fit rounded-md absolute bg-[#D6D7E0] text-gray-800 left-12 top-1 text-xs p-2 hidden group-hover:block">
-            Tasks
-          </div>
-          <GoTasklist size={18} />
-        </button>
+        {navItems.map(({ href, icon: Icon, name }) => {
+          return (
+            <Link
+              key={name}
+              href={href}
+              className={`rounded-lg p-3 relative group ${
+                href === pathname ? "bg-[#d9d9d9]" : ""
+              }`}
+            >
+              <div className="w-fit h-fit rounded-md absolute bg-[#d9d9d9] left-12 top-1 text-xs p-2 hidden group-hover:block">
+                {name}
+              </div>
+              <Icon size={18} />
+            </Link>
+          );
+        })}
       </nav>
     </div>
   );
