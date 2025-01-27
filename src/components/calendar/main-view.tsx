2d4c4a08945bd2b2fd5b32bae36b98457d937403
subhash131 <subhashnayak131@gmail.com>
2025-01-27 21:51:25 +0530

    update calendar layout

--- src/components/calendar/main-view.tsx
+++ src/components/calendar/main-view.tsx
@@ -1,48 +1,43 @@
+"use client";
 import React from "react";
 import MonthView from "./month-view";
 import Sidebar from "../sidebar";
 import Summary from "./summary";
 import WeekView from "./week-view";
-import {
-  IoIosArrowBack,
-  IoIosArrowDown,
-  IoIosArrowForward,
-} from "react-icons/io";
+import Header from "./header";
+import { useSelector } from "react-redux";
+import { RootState } from "@/state-manager/store";
+import WeekViewNew from "./week-view-new";
+import { getWeekDays } from "@/utils/get-time";
+import dayjs from "dayjs";
 
 const MainView = () => {
+  const daysInWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const days = getWeekDays(dayjs());
+  const { view } = useSelector((state: RootState) => state.calendar);
   return (
-    <div className="w-full h-screen flex pr-10">
+    <div className="w-full h-screen overflow-hidden flex pr-10">
       <Sidebar />
       <Summary />
-      <div className="size-full flex justify-center items-center flex-col pl-4">
-        <div className="h-24 w-full flex items-center justify-between">
-          <div className="rounded-md items-center flex gap-4">
-            <p className="px-4 border rounded-md py-1">Today</p>
-            <button className="p-1 rounded-full border border-white hover:bg-[#F4F5F7] hover:border-[#D6D7E0]">
-              <IoIosArrowBack />
-            </button>
-            <button className="p-1 rounded-full border border-white hover:bg-[#F4F5F7] hover:border-[#D6D7E0]">
-              <IoIosArrowForward />
-            </button>
-            <p className="font-semibold">Jan - Feb 2024</p>
-          </div>
-          <div className="px-4 py-1 rounded-md border relative group">
-            <p className="flex items-center gap-2">
-              Week <IoIosArrowDown />
-            </p>
-            <div className="absolute bg-white border rounded-md hidden group-hover:flex flex-col items-center justify-center top-8 left-0 w-full">
-              <p className="border-b size-full flex items-center justify-center py-1">
-                Week
-              </p>
-              <p className="size-full flex items-center justify-center py-1">
-                Month
-              </p>
+      <div className="h-fit w-full flex justify-center items-center flex-col pl-4">
+        <Header />
+        <div className="w-full border-[0.5px]">
+          {view === "Month" && <MonthView />}
+          {/* {view === "Week" && <WeekView />} */}
+          {view === "Week" && (
+            <div className="size-full relative">
+              <div className="sticky top-0 w-full h-16 pl-20 flex">
+                {days.map((day, i) => {
+                  return (
+                    <div className="size-full flex items-center justify-center border-[0.5px]">
+                      {`${daysInWeek[i]} ${day.currentDate.toDate().getDate()}`}
+                    </div>
+                  );
+                })}
+              </div>
+              <WeekViewNew />
             </div>
-          </div>
-        </div>
-        <div className="size-full">
-          {/* <MonthView /> */}
-          <WeekView />
+          )}
         </div>
         <div className="h-24 w-full"></div>
       </div>
--- src/components/calendar/week-view.tsx
+++ src/components/calendar/week-view.tsx
@@ -7,30 +7,35 @@ const WeekView = () => {
   const days = getWeekDays(dayjs());
   console.log(days);
   return (
-    <section className="size-full">
-      <div className="flex items-start justify-evenly w-full h-fit border-b-0 border-[0.5px]">
-        {days.map((day, index) => {
-          return (
-            <div
-              className="w-full py-4 flex items-center justify-center border-[0.5px] gap-2"
-              key={day.currentDate.toDate().getDate() + index}
-            >
-              <p>{`${weekDays[index]} ${day.currentDate
-                .toDate()
-                .getDate()}`}</p>
-            </div>
-          );
-        })}
+    <section className="size-full flex border-[0.5px]">
+      <div className="w-20 h-full border-[0.5px] mt-10">
+        <div className="size-full"></div>
       </div>
-      <div className="flex items-start justify-evenly size-full border-[0.5px] border-t-0">
-        {weekDays.map((_, index) => {
-          return (
-            <div
-              className="w-full h-full py-4 flex items-center justify-center border-[0.5px] flex-col"
-              key={index}
-            ></div>
-          );
-        })}
+      <div className="size-full">
+        <div className="flex items-start justify-evenly w-full h-fit border-[0.5px]">
+          {days.map((day, index) => {
+            return (
+              <div
+                className="w-full h-10 flex items-center justify-center border-[0.5px] gap-2"
+                key={day.currentDate.toDate().getDate() + index}
+              >
+                <p>{`${weekDays[index]} ${day.currentDate
+                  .toDate()
+                  .getDate()}`}</p>
+              </div>
+            );
+          })}
+        </div>
+        <div className="flex items-start justify-evenly size-full border-[0.5px] border-t-0">
+          {weekDays.map((_, index) => {
+            return (
+              <div
+                className="w-full h-full py-4 flex items-center justify-center border-[0.5px] flex-col"
+                key={index}
+              ></div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
