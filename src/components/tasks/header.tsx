8b5dfa20cd08b0cae625c8436c062a970bc9d65c
subhash131 <subhashnayak131@gmail.com>
2025-01-29 03:05:46 +0530

    update task

--- src/components/tasks/header.tsx
+++ src/components/tasks/header.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { BiPlus } from "react-icons/bi";
 
-const Header = () => {
+const Header = ({ projects }: any) => {
   return (
     <div className="w-full h-12 flex items-center justify-between gap-2 rounded-t-lg px-4">
       <div className="flex gap-4 items-center text-sm">
-        <button className="py-1 transition-colors border-b border-white hover:border-[#c4c4c4] flex items-center">
-          Project 1
-        </button>
-        <button
-          className={`py-1 transition-colors border-b border-white hover:border-[#c4c4c4] bg-white flex items-center ${
-            true ? "border-[#c4c4c4]" : ""
-          }`}
-        >
-          Project 2
-        </button>
+        {projects.map(({ name, isActive }: any) => {
+          return (
+            <button
+              className={`py-1 text-nowrap transition-colors border-b border-white hover:border-[#c4c4c4] flex items-center ${
+                isActive ? "border-[#9f9f9f]" : ""
+              }`}
+              key={"head" + name}
+            >
+              {name}
+            </button>
+          );
+        })}
       </div>
       <button className="px-2 py-1 rounded-md transition-colors border border-white hover:border-[#E4E7EB] hover:bg-[#F9FAFC] bg-white flex items-center gap-1 text-xs">
         <BiPlus size={16} /> New Project
