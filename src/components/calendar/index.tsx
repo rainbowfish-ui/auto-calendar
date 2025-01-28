9f5172d712ff930e9229261a4c0d1b3e3a913535
subhash131 <subhashnayak131@gmail.com>
2025-01-29 03:57:47 +0530

    fix build bug

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Header from "./header";
 import ActiveView from "./active-view";
 
 const Calendar = () => {
   return (
     <div className="h-fit w-full flex justify-center items-center flex-col pl-4">
-      <Header />
-      <ActiveView />
-      <div className="h-24 w-full"></div>
+      <Suspense>
+        <Header />
+        <ActiveView />
+      </Suspense>
     </div>
   );
 };
