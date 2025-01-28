adba1c433e12ce745e8fc2f211c10747a6628260
subhash131 <subhashnayak131@gmail.com>
2025-01-29 03:05:09 +0530

    update project

--- src/components/tasks/project/header.tsx
+++ src/components/tasks/project/header.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { IoIosMore } from "react-icons/io";
 
-const Header = () => {
+const Header = ({ name }: { name: string }) => {
   return (
     <div className="border-b h-10 font-semibold w-full flex items-center justify-between px-4">
-      <p>Design</p>
+      <p>{name}</p>
       <IoIosMore size={20} />
     </div>
   );
--- src/components/tasks/project/index.tsx
+++ src/components/tasks/project/index.tsx
@@ -1,15 +1,37 @@
 import React from "react";
 import Header from "./header";
 import TaskCard from "./task-card";
+import DropArea from "./drop-area";
 
-const Project = () => {
+const Project = ({
+  name,
+  tasks,
+  members,
+  setActiveCard,
+}: {
+  name: string;
+  members: any;
+  tasks: any;
+  setActiveCard: any;
+}) => {
   return (
     <div className="bg-[#F9FAFC] h-fit w-72 border rounded-md shrink-0 text-sm">
-      <Header />
-      <div className="w-full h-full flex flex-col items-center justify-start gap-2 p-2">
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
+      <Header name={name} />
+      <div className="w-full h-full flex flex-col items-center justify-start p-2">
+        <DropArea />
+        {tasks.map((task: any, i: number) => {
+          return (
+            <React.Fragment key={task.name}>
+              <TaskCard
+                index={i}
+                members={task.members}
+                name={task.name}
+                setActiveCard={setActiveCard}
+              />
+              <DropArea />
+            </React.Fragment>
+          );
+        })}
       </div>
     </div>
   );
