ad1336c0c4fe12e443d26c8eb0f635dcfb265de8
subhash131 <subhashnayak131@gmail.com>
2025-01-29 03:50:03 +0530

    add drag and drop functionality

--- src/components/tasks/project/drop-area.tsx
+++ src/components/tasks/project/drop-area.tsx
@@ -1,19 +1,29 @@
 "use client";
 import React, { useState } from "react";
 
-const DropArea = () => {
+const DropArea = ({
+  index,
+  teamIndex,
+  onDrop,
+  activeCard,
+}: {
+  teamIndex: number;
+  index: number;
+  onDrop: any;
+  activeCard: any;
+}) => {
   const [showDrop, setShowDrop] = useState(false);
   return (
     <section
       className={`w-full transition-all flex items-center justify-center text-gray-600 ${
         showDrop
           ? "h-36 border border-gray-600 border-dashed p-2 rounded-md"
-          : "h-4 opacity-0"
+          : "h-5 opacity-0"
       }`}
       onDragEnter={() => setShowDrop(true)}
       onDragLeave={() => setShowDrop(false)}
       onDrop={(e) => {
-        console.log("target", e.target);
+        onDrop({ ...activeCard }, { toTeam: teamIndex, index });
         setShowDrop(false);
       }}
       onDragOver={(e) => e.preventDefault()}
--- src/components/tasks/project/task-card.tsx
+++ src/components/tasks/project/task-card.tsx
@@ -4,11 +4,13 @@ import React from "react";
 const TaskCard = ({
   members,
   name,
+  teamIndex,
   setActiveCard,
   index,
 }: {
   members: any;
   name: string;
+  teamIndex: number;
   setActiveCard: any;
   index: number;
 }) => {
@@ -16,7 +18,7 @@ const TaskCard = ({
     <article
       className="w-full h-36 shadow-md border rounded-md bg-white p-1 active:opacity-50 active:border-black active:cursor-grabbing"
       draggable
-      onDrag={() => setActiveCard(index)}
+      onDrag={() => setActiveCard({ fromTeam: teamIndex, index })}
       onDragEnd={() => setActiveCard(null)}
     >
       <p className="text-xs font-semibold text-center">{name}</p>
