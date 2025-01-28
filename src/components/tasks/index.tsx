ca99604ecd0fe46581bf9ec018781eb1324e3be9
subhash131 <subhashnayak131@gmail.com>
2025-01-29 03:51:35 +0530

    remove logs

--- src/components/tasks/index.tsx
+++ src/components/tasks/index.tsx
@@ -10,32 +10,26 @@ const Tasks = () => {
     from: { fromTeam: number; index: number },
     to: { toTeam: number; index: number }
   ) {
-    console.log("F", from);
-    console.log("T", to);
     const project = projects.find((proj) => proj.isActive);
-    console.log("project---", project);
+
     if (!project) return;
     const sourceTeam = project.teams[from.fromTeam];
-    console.log("sourceTeam---", sourceTeam);
     const destinationTeam = project.teams[to.toTeam];
-    console.log("destinationTeam---", destinationTeam);
 
     if (!sourceTeam || !destinationTeam) {
-      console.log("Teams not found");
+      console.log("Team not found");
     }
     const [movedTask] = sourceTeam.tasks.splice(from.index, 1);
-
     if (!movedTask) {
-      console.log("Teams not found");
+      console.log("Error");
     }
     destinationTeam.tasks.splice(to.index, 0, movedTask);
-    console.log({ ...project, teams: [...project.teams] });
+
     return { ...project, teams: [...project.teams] };
   }
   return (
     <div className="h-screen w-fit pl-4">
       <Header projects={projects} />
-      <div>Aaaaa--{JSON.stringify(activeCard)}</div>
       <div className="h-screen w-[70vw] flex overflow-scroll gap-4 p-2 no-scrollbar pb-44">
         {projects.map(({ isActive, name: projectName, teams }) => {
           return (
