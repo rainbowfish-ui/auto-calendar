5ec2fce3161253f6f8fc217cb1c98b566ce1a76a
subhash131 <subhashnayak131@gmail.com>
2025-01-31 00:19:18 +0530

    update task

--- src/components/tasks/header/index.tsx
+++ src/components/tasks/header/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BiPlus } from "react-icons/bi";
 import ProjectList from "./project-list";
 
-const Header = ({ projects }: any) => {
+const Header = () => {
   return (
     <div className="w-full h-12 flex items-center justify-between gap-2 rounded-t-lg px-4">
       <ProjectList />
@@ -10,20 +10,7 @@ const Header = ({ projects }: any) => {
         <button className="px-2 py-1 rounded-md transition-transform border border-[#E4E7EB] bg-[#F9FAFC] flex items-center gap-1 active:scale-95">
           <BiPlus size={16} /> Team
         </button>
-        <button
-          className="px-2 py-1 rounded-md transition-transform border border-[#E4E7EB] bg-[#F9FAFC] flex items-center gap-1 active:scale-95"
-          onClick={() => {
-            projects.push({
-              name: "vectal.ai",
-              isActive: true,
-              teams: [
-                {
-                  name: "Design Team",
-                },
-              ],
-            });
-          }}
-        >
+        <button className="px-2 py-1 rounded-md transition-transform border border-[#E4E7EB] bg-[#F9FAFC] flex items-center gap-1 active:scale-95">
           <BiPlus size={16} /> Project
         </button>
       </div>
