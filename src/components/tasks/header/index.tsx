38cb6b8ca6938876332142bfb905ecd8ad3d5ef7
subhash131 <subhashnayak131@gmail.com>
2025-01-31 01:40:41 +0530

    update task header

--- src/components/tasks/header/index.tsx
+++ src/components/tasks/header/index.tsx
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiPlus } from "react-icons/bi";
 import ProjectList from "./project-list";
+import NewProject from "@/components/new-project";
+import NewTeam from "@/components/new-team";
 
 const Header = () => {
+  const [isNewTeamModalOpen, setIsNewTeamModalOpen] = useState(false);
+  const [isNewProjectModalOpen, setIsNewProjectModalOpen] = useState(false);
   return (
     <div className="w-full h-12 flex items-center justify-between gap-2 rounded-t-lg px-4">
       <ProjectList />
       <div className="flex gap-2 items-center text-xs">
-        <button className="px-2 py-1 rounded-md transition-transform border border-[#E4E7EB] bg-[#F9FAFC] flex items-center gap-1 active:scale-95">
+        <button
+          className="px-2 py-1 rounded-md transition-transform border border-[#E4E7EB] bg-[#F9FAFC] flex items-center gap-1 active:scale-95"
+          onClick={() => setIsNewTeamModalOpen(true)}
+        >
           <BiPlus size={16} /> Team
         </button>
-        <button className="px-2 py-1 rounded-md transition-transform border border-[#E4E7EB] bg-[#F9FAFC] flex items-center gap-1 active:scale-95">
+        <button
+          className="px-2 py-1 rounded-md transition-transform border border-[#E4E7EB] bg-[#F9FAFC] flex items-center gap-1 active:scale-95"
+          onClick={() => setIsNewProjectModalOpen(true)}
+        >
           <BiPlus size={16} /> Project
         </button>
       </div>
+      <NewProject
+        isNewProjectModalOpen={isNewProjectModalOpen}
+        setIsNewProjectModalOpen={setIsNewProjectModalOpen}
+      />
+      <NewTeam
+        isNewTeamModalOpen={isNewTeamModalOpen}
+        setIsNewTeamModalOpen={setIsNewTeamModalOpen}
+      />
     </div>
   );
 };
--- src/components/tasks/index.tsx
+++ src/components/tasks/index.tsx
@@ -3,9 +3,22 @@ import React, { useState } from "react";
 import Project from "./project";
 import Header from "./header";
 import { projects } from "@/db/projects";
+import { useSelector } from "react-redux";
+import { RootState } from "@/state-manager/store";
+import { useQuery } from "@tanstack/react-query";
+import { getProjectById } from "@/app/actions/get-project-by-id";
+import { PiSpinnerLight } from "react-icons/pi";
 
 const Tasks = () => {
   const [activeCard, setActiveCard] = useState(null);
+  const { activeProject } = useSelector((state: RootState) => state.projects);
+
+  const { data: project, isLoading } = useQuery({
+    queryKey: ["active-project", activeProject],
+    queryFn: () => getProjectById(activeProject),
+    enabled: !!activeProject,
+  });
+
   function onDrop(
     from: { fromTeam: number; index: number },
     to: { toTeam: number; index: number }
@@ -31,29 +44,27 @@ const Tasks = () => {
     <div className="h-screen w-fit pl-4">
       <Header />
       <div className="h-screen w-[70vw] flex overflow-scroll gap-4 p-2 no-scrollbar pb-44">
-        {projects.map(({ isActive, name: projectName, teams }) => {
-          return (
-            isActive && (
-              <React.Fragment key={projectName}>
-                {teams.map(({ members, name, tasks }, index) => {
-                  return (
-                    <Project
-                      setActiveCard={setActiveCard}
-                      name={name}
-                      members={members}
-                      tasks={tasks}
-                      key={name + index}
-                      projectName={projectName}
-                      onDrop={onDrop}
-                      activeCard={activeCard}
-                      teamIndex={index}
-                    />
-                  );
-                })}
-              </React.Fragment>
-            )
-          );
-        })}
+        {isLoading && (
+          <div className="size-full grid place-content-center">
+            <PiSpinnerLight className="animate-spin" size={30} />
+          </div>
+        )}
+        {project &&
+          project.teams?.map(({ members, name, tasks }: any, index: number) => {
+            return (
+              <Project
+                setActiveCard={setActiveCard}
+                name={name}
+                members={members}
+                tasks={tasks}
+                key={name + index}
+                projectName={project.name}
+                onDrop={onDrop}
+                activeCard={activeCard}
+                teamIndex={index}
+              />
+            );
+          })}
       </div>
     </div>
   );
