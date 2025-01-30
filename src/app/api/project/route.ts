d80453bf3d9a11037a9d1766fdc68436f0421532
subhash131 <subhashnayak131@gmail.com>
2025-01-31 04:00:49 +0530

    update project route

--- src/app/api/project/route.ts
+++ src/app/api/project/route.ts
@@ -3,21 +3,24 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
   const projectId = req.nextUrl.searchParams.get("projectId");
-
   if (!projectId) {
     return NextResponse.json(
       { error: "projectId is required" },
       { status: 400 }
     );
   }
-
   const project = projects.find(
     (project) => project.id.toString() === projectId
   );
-
   if (!project) {
     return NextResponse.json({ error: "Project not found" }, { status: 404 });
   }
-
   return NextResponse.json(project);
+};
+
+
+export const POST = async (req: NextRequest) => {
+  const reqJson = await req.json();
+  projects.push(reqJson);
+  return NextResponse.json({ msg: "New project created!!" });
 };
