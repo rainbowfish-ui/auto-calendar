426dfeae5decebb38c1b2150778666ae556fb68c
subhash131 <subhashnayak131@gmail.com>
2025-01-30 21:38:32 +0530

    update metadata

--- src/app/(protected)/calendar/page.tsx
+++ src/app/(protected)/calendar/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import Calendar from "@/components/calendar";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Auto Calendar",
+  description: "Project by subhash",
+};
 
 const CalendarPage = () => {
   return <Calendar />;
