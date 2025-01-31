7644d7e82f6f908ffb9a2b792a68a32a8baea893
subhash131 <subhashnayak131@gmail.com>
2025-01-31 23:07:30 +0530

    update projects db connection

--- src/app/api/project/route.ts
+++ src/app/api/project/route.ts
@@ -1,4 +1,6 @@
+import { connectToDatabase } from "@/db/config";
 import { projects } from "@/db/projects";
+import { ObjectId } from "mongodb";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
@@ -9,18 +11,44 @@ export const GET = async (req: NextRequest) => {
       { status: 400 }
     );
   }
-  const project = projects.find(
-    (project) => project.id.toString() === projectId
-  );
+  const db = await connectToDatabase();
+  const projectsCollection = db.collection("projects");
+
+  const project = await projectsCollection.findOne({
+    _id: new ObjectId(projectId),
+  });
+
   if (!project) {
     return NextResponse.json({ error: "Project not found" }, { status: 404 });
   }
   return NextResponse.json(project);
 };
 
-
 export const POST = async (req: NextRequest) => {
-  const reqJson = await req.json();
-  projects.push(reqJson);
-  return NextResponse.json({ msg: "New project created!!" });
+  try {
+    const reqJson = await req.json();
+
+    if (!reqJson.name || !reqJson.description) {
+      return NextResponse.json(
+        { error: "Name and description are required" },
+        { status: 400 }
+      );
+    }
+
+    const db = await connectToDatabase();
+    const projectsCollection = db.collection("projects");
+
+    const result = await projectsCollection.insertOne(reqJson);
+
+    return NextResponse.json({
+      msg: "New project created!",
+      projectId: result.insertedId,
+    });
+  } catch (error) {
+    console.error("Error creating project:", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
 };
