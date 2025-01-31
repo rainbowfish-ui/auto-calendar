b8d5d9d6cc41d1bfd1d9adcb71d6de3a1a5dad72
subhash131 <subhashnayak131@gmail.com>
2025-01-31 22:01:08 +0530

    update component: new task

--- src/components/new-task/index.tsx
+++ src/components/new-task/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Modal from "@/components/modal"; // Adjust path if necessary
 import Name from "./name";
-import AddTeam from "./add-team";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createNewProject } from "@/actions/create-new-project";
 import { useSelector } from "react-redux";
@@ -26,6 +25,8 @@ export default function NewTask({
     mutationFn: createNewProject,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["project-names"] });
+      toast.success("New task created");
+      setIsNewTaskModalOpen(false);
     },
     onError: (error) => {
       console.error("Failed to create project:", error);
@@ -37,21 +38,19 @@ export default function NewTask({
       isOpen={isNewTaskModalOpen}
       onClose={() => setIsNewTaskModalOpen(false)}
     >
-      <div className="w-[60vw] h-[70vh] flex flex-col p-4 text-xs gap-6 overflow-y-scroll no-scrollbar">
-        <p className="text-lg font-semibold">Create new project</p>
-        <div className="flex gap-10 items-center">
-          <Name />
+      <div className="w-[60vw] h-[70vh] flex flex-col p-4 text-xs gap-6 overflow-y-scroll no-scrollbar justify-between">
+        <div>
+          <p className="text-lg font-semibold">Add new task</p>
+          <div className="flex gap-10 items-center mt-4">
+            <Name />
+          </div>
         </div>
-        <div className="flex gap-10 items-center">
-          <AddTeam />
-        </div>
+        <div className="flex gap-10 items-center"></div>
         <button
           className="px-4 py-2 bg-[#F9FAFC] border rounded-md active:scale-95 transition-transform font-semibold"
           onClick={() => {
             const id = generateRandomId({ length: 6 });
             handleCreate({ name, teams, id });
-            toast.success("New project created");
-            setIsNewTaskModalOpen(false);
           }}
         >
           Create
