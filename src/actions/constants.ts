948ed31b159d644f77159d12921a2e0eeccba4b6
subhash131 <subhashnayak131@gmail.com>
2025-02-01 02:14:18 +0530

    create action: createNewTeam

--- src/actions/constants.ts
+++ src/actions/constants.ts
@@ -1,2 +1,1 @@
-"use server";
 export const BASE_URL = process.env.BASE_URL;
---
+++ src/actions/create-new-team.ts
@@ -0,0 +1,23 @@
+"use server";
+import { BASE_URL } from "./constants";
+
+export const createNewTeam = async (body: any) => {
+  try {
+    const response = await fetch(`${BASE_URL}/api/project`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error("Failed to create project:", error);
+    throw error;
+  }
+};
