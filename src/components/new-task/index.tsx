91f3d7012ee40f7cc09728ec07e603fd543842cc
subhash131 <subhashnayak131@gmail.com>
2025-02-01 23:56:27 +0530

    update new-task form

--- src/components/new-task/index.tsx
+++ src/components/new-task/index.tsx
@@ -3,10 +3,12 @@ import Modal from "@/components/modal"; // Adjust path if necessary
 import Name from "./name";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createNewProject } from "@/actions/create-new-project";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/state-manager/store";
 import { toast } from "sonner";
 import Context from "./context";
+import Priority from "./priority";
+import { setPriority, TaskForm } from "@/state-manager/features/new-task-form";
 
 export default function NewTask({
   isNewTaskModalOpen,
@@ -16,6 +18,7 @@ export default function NewTask({
   setIsNewTaskModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
   const { name } = useSelector((state: RootState) => state.newTaskForm);
+  const dispatch = useDispatch();
 
   const queryClient = useQueryClient();
 
@@ -31,6 +34,15 @@ export default function NewTask({
     },
   });
 
+  const handleClick = (e: React.MouseEvent) => {
+    const action = (e.target as HTMLElement).getAttribute("data-action");
+    console.log(action);
+    console.log("assas");
+    if (action === "create-task") {
+      handleCreate({ name });
+    }
+  };
+
   return (
     <Modal
       isOpen={isNewTaskModalOpen}
@@ -39,17 +51,16 @@ export default function NewTask({
       <div className="w-96 h-[70vh] flex flex-col p-4 text-xs gap-6 overflow-y-scroll no-scrollbar justify-between">
         <div>
           <p className="text-lg font-semibold">Add new task</p>
-          <div className="flex gap-10 items-center mt-4">
+          <div className="flex flex-col gap-2 items-start size-full mt-4">
             <Name />
             <Context />
+            <Priority />
           </div>
         </div>
-        <div className="flex gap-10 items-center"></div>
         <button
           className="px-4 py-2 bg-[#F9FAFC] border rounded-md active:scale-95 transition-transform font-semibold"
-          onClick={() => {
-            handleCreate({ name });
-          }}
+          onClick={handleClick}
+          data-action="create-task"
         >
           Create
         </button>
---
+++ src/components/new-task/priority.tsx
@@ -0,0 +1,46 @@
+"use client";
+import React from "react";
+import { RootState } from "@/state-manager/store";
+import { useDispatch, useSelector } from "react-redux";
+import { setPriority, TaskForm } from "@/state-manager/features/new-task-form";
+import { FaCheck } from "react-icons/fa";
+
+const Priority = () => {
+  const priorities: TaskForm["priority"][] = ["LOW", "MEDIUM", "HIGH"];
+  const { priority: currentPriority } = useSelector(
+    (state: RootState) => state.newTaskForm
+  );
+  const dispatch = useDispatch();
+  const handleClick = (e: React.MouseEvent) => {
+    const action = (e.target as HTMLElement).getAttribute("data-action");
+    if (action === "set-priority") {
+      const priority = (e.target as HTMLElement).getAttribute("data-priority");
+      if (!priority) return;
+      dispatch(setPriority(priority as TaskForm["priority"]));
+    }
+  };
+  return (
+    <div className="flex flex-col gap-1" onClick={handleClick}>
+      <p className="font-semibold ">Priority</p>
+      <div className="w-full flex gap-2 items-center">
+        {priorities.map((priority) => {
+          return (
+            <button
+              className={`px-2 py-1 border rounded-lg flex items-center gap-1 active:scale-95 transition-transform ${
+                priority === currentPriority ? "bg-[#F9FAFC]" : "bg-white"
+              }`}
+              key={priority}
+              data-priority={priority}
+              data-action="set-priority"
+            >
+              {priority}
+              {priority === currentPriority && <FaCheck />}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Priority;
